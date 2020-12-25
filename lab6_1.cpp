167e7eb683245744f9c4e3102d5b463c74cff0a5
iolq <wipawee_watt@cmu.ac.th>
2020-12-25 15:33:03 +0700

    completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 
-int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
+using namespace std;
 
-    outpot = a - b/c;
-    count << output: << output << \n;
+int main() {
+    double a  =  20; 
+	double b = 30;
+    double c = 4;
+	double output = 0;
+
+    output = (a+b)/c;
+    cout << "output:" << output << "\n";
     return 0;
 }
